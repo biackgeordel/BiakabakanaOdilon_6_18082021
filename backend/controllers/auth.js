4b8a2cebf82e643465f94e195f8c4925886d0776
Biakabakana odilon <biackgeordel@gmail.com>
2021-09-14 07:41:04 +0200

    mise à jour du projet6 final

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -2,7 +2,7 @@ const jwt=require('jsonwebtoken');
 module.exports=(req,res,next)=>{
     try{
         const token=(req.headers.authorization).split(' ')[1];
-        //convert le tabToken en objet avec la fonction verify de jspnwebtoken
+        //convert le tabToken en objet avec la fonction verify de jsonwebtoken
         const tabToken=jwt.verify(token,"RANDOM_TOKEN_SECRET");
         console.log(tabToken);
         /*on recupére l'id user stocké dans tabToken et on
